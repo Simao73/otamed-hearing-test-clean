1f946f550f4bbcbd4e98f592180befe927ebeb84
Simao73 <ailiades@otenet.gr>
2025-06-20 19:27:23 +0300

    Update EarButton.tsx

--- components/EarButton.tsx
+++ components/EarButton.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 interface Props {
   stage: "start" | "ready" | "testing" | "done";
   setStage: (stage: "start" | "ready" | "testing" | "done") => void;
+
   setLowFreq: (freq: number) => void;
   setHighFreq: (freq: number) => void;
   language: "en" | "gr";
